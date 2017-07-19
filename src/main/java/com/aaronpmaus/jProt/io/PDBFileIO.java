edcf136a71f78890f41c5d7eff6fa8811f48672f
Aaron Maus <amaus@uno.edu>
2017-07-19 11:11:52 -0500

    adding initial PDBFileIO class. This class will read and write to PDB files

---
+++ src/main/java/com/aaronpmaus/jProt/io/PDBFileIO.java
@@ -0,0 +1,25 @@
+package com.aaronpmaus.jProt.io;
+
+import com.aaronpmaus.jProt.protein.*;
+import java.util.ArrayList;
+
+/**
+ * Provides the ability to read in and write out PDB Files
+ * @author Aaron Maus aaron@aaronpmaus.com
+ * @version 0.6.0
+ * @since 0.6.0
+*/
+public class PDBFileIO{
+    ArrayList<String> fileLines;
+
+
+    public PDBFileIO(){
+        fileLines = new ArrayList<String>();
+    }
+
+    public Protein readInPDBFile(String fileName){
+        return null;
+    }
+
+
+}
